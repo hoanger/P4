8e61b50382ee7cbd7b06c3e94f3d40603f5e7138
Andy Nguyen <nguyenandyh@gmail.com>
2015-11-16 23:16:30 -0800

    refactor: Move scroll position calculation outside loop in updatePositions

--- prod/views/js/main.js
+++ prod/views/js/main.js
@@ -508,8 +508,10 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  // Calculate scroll position outside for loop to prevent concurrent read and write cycles
+  var scrollPos = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((scrollPos / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
